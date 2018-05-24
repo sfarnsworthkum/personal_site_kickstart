39a0a6672bce44b66e98b14946b3fef9e1d3a285
Sarah Farnsworth-kumli <sarah@stormpath.com>
2018-05-23 17:11:33 -0700

    took out hardcoding

--- build.py
+++ build.py
@@ -1,9 +1,10 @@
+import sys
 top_content = open('templates/top.html').read()
-middle_content = open('content/index.html').read()
+middle_content = open(sys.argv[1]).read()
 bottom_content = open('templates/bottom.html').read()
 
 all_files = top_content + middle_content + bottom_content
-output = open('docs/index.html', 'w')
+output = open('docs/' + sys.argv[2], 'w')
 output.write(all_files)
 
 
