37e6c52c0428c7ef1198aaaa1da167b31bd8b418
Sarah Farnsworth-kumli <sarah@stormpath.com>
2018-05-31 16:17:11 -0700

    bold current link in navbar

--- build.py
+++ build.py
@@ -1,40 +1,34 @@
-
-'''
-Must have a Python script named “build.py” that generates the site
-• All code in Python script must be in functions – no “loose” code (with the exception of the initial invocation of the “main()” function, see below)
-• Python script must use lists to store information about all pages
-• Python script must use at least one for loop to process all pages
-• You must create and use at least 3 functions (it’s up to you how you separate your code into functions)
-• As with before, all code must be submitted as a Pull Request
-• Don’t worry about the build.sh file – you can delete that. From now on your site is Python only.
-'''
 def main():
     all_files = [
         {
             'file_name': 'about',
-            'title': 'About Me'
+            'title': 'About Me',
+            'css_id': 'about'
         },
         {
             'file_name': 'blog',
-            'title': 'Blog'
+            'title': 'Blog',
+            'css_id': 'blog'
         },
         {
             'file_name': 'index',
-            'title': 'Sarah Farnsworth-Kumli'
+            'title': 'Sarah Farnsworth-Kumli',
+            'css_id' : 'index'
         },
         {
             'file_name': 'projects',
-            'title': 'My Projects'
+            'title': 'My Projects',
+            'css_id': 'projects'
         },
     ]
     for item in all_files:
-        create_file(item['file_name'], item['title'])
+        create_file(item['file_name'], item['title'], item['css_id'])
 
 
-def create_file(file_name, title):
+def create_file(file_name, title, css_id):
     layout = open('templates/layout.html').read()
     middle_content = open('content/' + file_name + '.html').read()
-    finished_page = layout.replace('{{content}}', middle_content).replace('{{title}}', title)
+    finished_page = layout.replace('{{content}}', middle_content).replace('{{title}}', title).replace('{{id}}', css_id)
     output = open('docs/' + file_name + '.html', 'w')
     output.write(finished_page)
 
